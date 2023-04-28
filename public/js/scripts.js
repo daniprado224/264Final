a344b982d65a2682ec195ae616d04a2d6f77f30e
epa224 <epa224@lehigh.edu>
2023-04-28 16:29:14 -0400

    file structure reorganized

---
+++ public/js/scripts.js
@@ -0,0 +1,12 @@
+/* 
+Elyse Adamo
+Daniella Prado 
+Luisa Slomp
+CSE 264- Final Project: CrossWord Puzzle 
+This file is for the backend of the crossword puzzle */
+
+$(function () {
+    $("#easy").click((evt) =>{  
+      console.log("here");
+    });
+  });
